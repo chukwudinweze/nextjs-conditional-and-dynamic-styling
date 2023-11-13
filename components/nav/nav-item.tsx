b168434a006478b7cd1004cc9f3dbe999f6e7845
Chukwudi Nweze <45886893+chukwudinweze@users.noreply.github.com>
2023-11-13 14:06:30 +0100

    created dynamic nav item

---
+++ components/nav/nav-item.tsx
@@ -0,0 +1,32 @@
+import { IconType } from "react-icons";
+
+interface NavItemProps {
+  icon?: IconType;
+  inputType: "text" | "check";
+  placeholder?: string;
+}
+
+const NavItem = ({ icon: Icon, inputType, placeholder }: NavItemProps) => {
+  return (
+    <div className="flex justify-between flex-1 items-center px-2">
+      {inputType === "text" && Icon && <Icon />}
+      {inputType === "text" && (
+        <input
+          type="text"
+          placeholder={placeholder}
+          className="p-1 flex-1 placeholder:text-xs"
+        />
+      )}
+      {inputType === "check" && (
+        <input
+          type="checkbox"
+          placeholder="filter by size, companies or expertise"
+        />
+      )}
+      {inputType === "check" && <p>Full Time Only</p>}
+      {inputType === "check" && <button>hghhg</button>}
+    </div>
+  );
+};
+
+export default NavItem;
--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,36 +1,21 @@
 import { BiMap, BiSearch } from "react-icons/bi";
 import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
+import NavItem from "./nav-item";
 
 const Nav = () => {
   return (
-    <nav className=" flex justify-between lg:mx-20 divide-x-2 shadow-sm p-1 md:translate-y-[-50%] bg-white rounded-sm overflow-x-auto">
-      <div className="flex justify-between flex-1 items-center">
-        <BiSearch />
-        {/* search icon goes here */}
-        <input
-          type="text"
-          placeholder="filter by size, companies or expertise"
-          className="p-1 flex-1 "
-        />
-      </div>
-      <div className="flex flex-1 w-full items-center">
-        <BiMap />
-        {/* location icon goes here  */}
-        <input
-          type="text"
-          placeholder="filter by location"
-          className="flex-1 p-1"
-        />
-      </div>
-      <div className="flex flex-1 justify-between w-full items-center ">
-        <MdOutlineCheckBoxOutlineBlank />
-        {/* location icon goes here  */}
-        <input
-          type="text"
-          placeholder="filter by location"
-          className="flex-1 p-1 "
-        />
-      </div>
+    <nav className=" flex justify-between lg:mx-20 divide-x-2 shadow-sm p-1 py-3 md:translate-y-[-50%] bg-white rounded-sm overflow-x-auto">
+      <NavItem
+        icon={BiSearch}
+        inputType="text"
+        placeholder="filter by size, companies or expertise..."
+      />
+      <NavItem
+        icon={BiMap}
+        inputType="text"
+        placeholder="filter by location..."
+      />
+      <NavItem inputType="check" />
     </nav>
   );
 };
