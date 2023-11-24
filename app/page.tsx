1d127b580c0c9f95882a5b2a2ded59a79b735b70
Chukwudi Nweze <45886893+chukwudinweze@users.noreply.github.com>
2023-11-24 10:44:12 +0100

    modified page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,28 @@
+import { Button } from "@/components/button";
 import JobCard from "@/components/jobCard";
 import { jobs } from "@/data";
 
 const HomePage = () => {
   return (
-    <div className="flex justify-center mt-11">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-10 border w-full">
-        {jobs.map((job) => (
-          <JobCard
-            key={job.title}
-            company={job.company}
-            title={job.title}
-            time={job.time}
-            location={job.location}
-            logo={job.logo}
-            logoBg={job.logoBgColor}
-            type={job.type}
-          />
-        ))}
+    <div>
+      <div className="flex justify-center mt-11">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-10 border w-full">
+          {jobs.map((job) => (
+            <JobCard
+              key={job.title}
+              company={job.company}
+              title={job.title}
+              time={job.time}
+              location={job.location}
+              logo={job.logo}
+              logoBg={job.logoBgColor}
+              type={job.type}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="text-center w-28 mx-auto mt-8">
+        <Button variant="purple">Load more</Button>
       </div>
     </div>
   );
